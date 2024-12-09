48bbfcd6f4fe51dfbb788791a5281d280d026630
Artem Mizinin <mizinin@gmail.com>
2024-12-09 22:57:38 +0700

    uint->unsigned int

--- src/shader_recompiler/ir/passes/hull_shader_transform.cpp
+++ src/shader_recompiler/ir/passes/hull_shader_transform.cpp
@@ -175,7 +175,7 @@ std::optional<TessSharpLocation> FindTessConstantSharp(IR::Inst* read_const_buff
 class TessConstantUseWalker {
 public:
     void MarkTessAttributeUsers(IR::Inst* get_attribute) {
-        uint inc;
+        unsigned int inc;
         switch (get_attribute->Arg(0).Attribute()) {
         case IR::Attribute::TcsNumPatches:
         case IR::Attribute::TcsOutputBase:
@@ -196,7 +196,7 @@ public:
     }
 
 private:
-    void MarkTessAttributeUsersHelper(IR::Use use, uint inc) {
+    void MarkTessAttributeUsersHelper(IR::Use use, unsigned int inc) {
         IR::Inst* inst = use.user;
 
         switch (use.user->GetOpcode()) {
@@ -206,7 +206,7 @@ private:
         case IR::Opcode::WriteSharedU32:
         case IR::Opcode::WriteSharedU64:
         case IR::Opcode::WriteSharedU128: {
-            uint counter = inst->Flags<u32>();
+            unsigned int counter = inst->Flags<u32>();
             inst->SetFlags<u32>(counter + inc);
             // Stop here
             return;
@@ -252,7 +252,7 @@ private:
         }
     }
 
-    uint seq_num{1u};
+    unsigned int seq_num{1u};
 };
 
 enum class AttributeRegion : u32 { InputCP, OutputCP, PatchConst };
